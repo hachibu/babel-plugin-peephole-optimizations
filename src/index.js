8db87834798cfddddeee12aa3524aff562c33c37
hachibu <raymondsohn@gmail.com>
2020-06-10 15:27:34 -0700

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 export default babel => {
   let { types } = babel;
 
-  return {
-    visitor: {
-      BinaryExpression(path) {
-        let { node: { left, operator, right } } = path;
+  let visitor = {
+    BinaryExpression(path) {
+      let { node: { left, operator, right } } = path;
 
-        if (
-          left.type === 'StringLiteral' &&
-          right.type === 'StringLiteral' &&
-          operator === '+'
-        ) {
-          path.replaceWith(types.stringLiteral(left.value + right.value));
-        } else if (
-          left.type === 'NumericLiteral' &&
-          right.type === 'NumericLiteral' &&
-          operator === '+'
-        ) {
-          path.replaceWith(types.numericLiteral(left.value + right.value));
-        }
+      if (
+        left.type === 'StringLiteral' &&
+        right.type === 'StringLiteral' &&
+        operator === '+'
+      ) {
+        path.replaceWith(types.stringLiteral(left.value + right.value));
+      } else if (
+        left.type === 'NumericLiteral' &&
+        right.type === 'NumericLiteral' &&
+        operator === '+'
+      ) {
+        path.replaceWith(types.numericLiteral(left.value + right.value));
       }
     }
   };
+
+  return { visitor };
 };
