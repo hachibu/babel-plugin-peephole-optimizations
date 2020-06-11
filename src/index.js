68936306132e1b3f1eac3f1f516bd123a6c2eeb9
hachibu <raymondsohn@gmail.com>
2020-06-11 10:24:53 -0700

    Brute force tree recursion

--- src/index.js
+++ src/index.js
@@ -1,26 +1,62 @@
 export default babel => {
-  let { types } = babel;
-
   let visitor = {
     BinaryExpression(path) {
       if (!path.isBinaryExpression({ operator: '+' })) {
         return;
       }
 
-      let left = path.get('left');
+      let left  = path.get('left');
       let right = path.get('right');
 
-      if (left.isBinaryExpression()) {
-        left.traverse(visitor);
+      if (left.isBinaryExpression({ operator: '+' })) {
+        let leaf = getLeaf(left, left.key);
+        if (leaf) {
+          let typeCtor = getTypeCtor(leaf.node.type);
+          leaf.parentPath.replaceWith(
+            typeCtor(leaf.parent.left.value + leaf.parent.right.value)
+          );
+        }
       }
-      else if (left.isStringLiteral() && right.isStringLiteral()) {
-        path.replaceWith(types.stringLiteral(left.node.value + right.node.value));
+
+      if (left.isBinaryExpression({ operator: '+' })) {
+        let leaf = getLeaf(left, left.key);
+        if (leaf) {
+          let typeCtor = getTypeCtor(leaf.node.type);
+          leaf.parentPath.replaceWith(
+            typeCtor(leaf.parent.left.value + leaf.parent.right.value)
+          );
+        }
       }
-      else if (left.isNumericLiteral() && right.isNumericLiteral()) {
-        path.replaceWith(types.numericLiteral(left.node.value + right.node.value));
+
+      if (
+        (left.isStringLiteral() && right.isStringLiteral()) ||
+        (left.isNumericLiteral() && right.isNumericLiteral())
+      ) {
+        let typeCtor = getTypeCtor(left.node.type);
+        path.replaceWith(typeCtor(left.node.value + right.node.value));
       }
     }
   };
+
+  function getLeaf(path, direction) {
+    if (path.isNumericLiteral() ||path.isStringLiteral()) {
+      return path;
+    }
+    else if (path.isBinaryExpression({ operator: '+' })) {
+      return getLeaf(path.get(direction), direction);
+    }
+  }
+
+  function getTypeCtor(type) {
+    let typeCtor;
+    if (type === 'StringLiteral') {
+      typeCtor = babel.types.stringLiteral;
+    }
+    else if (type === 'NumericLiteral') {
+      typeCtor = babel.types.numericLiteral;
+    }
+    return typeCtor;
+  }
 
   return { visitor };
 };
