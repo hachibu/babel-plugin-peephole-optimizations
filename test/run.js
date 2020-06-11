cb842b67136b32b77b66b86eee11eb2f4a0be11a
hachibu <raymondsohn@gmail.com>
2020-06-11 12:25:46 -0700

    Refactor test/run.js

--- test/run.js
+++ test/run.js
@@ -4,27 +4,25 @@ import * as fs           from 'fs';
 import constantFolding   from '../lib/constant-folding';
 import strengthReduction from '../lib/strength-reduction';
 
-let testPaths = fs.readdirSync('test').
-  map(name => `test/${name}`).
-  filter(path => fs.lstatSync(path).isDirectory());
-
 describe('plugin', () => {
-  testPaths.forEach(testPath => {
-    let inputPath = `${testPath}/input.js`;
-    let outputPath = `${testPath}/output.js`;
+  let testPaths = fs.
+    readdirSync('test').
+    map(name => `test/${name}`).
+    filter(path => fs.lstatSync(path).isDirectory());
 
+  testPaths.forEach(testPath => {
     it(testPath, () => {
       let plugins = [];
 
-      if (inputPath.includes('constant-folding')) {
+      if (testPath.includes('constant-folding')) {
         plugins.push(constantFolding);
       }
-      else if (inputPath.includes('strength-reduction')) {
+      else if (testPath.includes('strength-reduction')) {
         plugins.push(strengthReduction);
       }
 
-      let input = babel.transformFileSync(inputPath, { plugins });
-      let output = fs.readFileSync(outputPath, 'utf-8').trim();
+      let input = babel.transformFileSync(`${testPath}/input.js`, { plugins });
+      let output = fs.readFileSync(`${testPath}/output.js`, 'utf-8').trim();
 
       assert.equal(input.code, output);
     });
